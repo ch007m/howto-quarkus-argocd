e4c94026a518bd4a563669e71a514fee02c66e93
cmoulliard <cmoulliard@redhat.com>
2025-01-22 18:16:38 +0100

    Include a retry function

--- test/setup-cluster.sh
+++ test/setup-cluster.sh
@@ -1,16 +1,30 @@
 #!/usr/bin/env bash
 
+retry() {
+    for _ in {1..3}; do
+        local ret=0
+        $1 || ret="$?"
+        if [[ "$ret" -eq 0 ]]; then
+            return 0
+        fi
+        sleep 3
+    done
+
+    echo "$1": "$2."
+    return "$ret"
+}
+
 export KIND_EXPERIMENTAL_PROVIDER=podman
 
 kind delete cluster
 kind create cluster --config $(pwd)/test/kind-port-80-443.cfg
 
 kubectl apply -f https://kind.sigs.k8s.io/examples/ingress/deploy-ingress-nginx.yaml
-kubectl wait --for=condition=Ready deployment/ingress-nginx-controller -n ingress-nginx --timeout=120s
+retry kubectl wait --for=condition=Ready deployment/ingress-nginx-controller -n ingress-nginx --timeout=30s
 
 kubectl create namespace argocd
 kubectl apply -n argocd -f https://raw.githubusercontent.com/argoproj/argo-cd/stable/manifests/install.yaml
-kubectl wait --for=condition=Ready deployment/argocd-server -n argocd --timeout=120s
+retry kubectl wait --for=condition=Ready deployment/argocd-server -n argocd --timeout=30s
 
 # See discussion: https://github.com/argoproj/argo-cd/issues/2953
 kubectl patch configmap argocd-cmd-params-cm \
@@ -20,12 +34,8 @@ kubectl patch configmap argocd-cmd-params-cm \
 
 kubectl rollout restart -n argocd deployment argocd-server
 kubectl apply -f $(pwd)/test/argocd-ingress.yaml
-sleep 20
 
-ARGOCD_ADMIN_PASSWORD=$(kubectl get secret/argocd-initial-admin-secret -n argocd -ojson | jq -r '.data.password' | base64 -d)
-echo "Argocd password: $ARGOCD_ADMIN_PASSWORD"
-
-argocd login argocd.localtest.me --grpc-web --insecure --username admin --password $ARGOCD_ADMIN_PASSWORD
+echo "## Cluster created and ready"
 
 
 
